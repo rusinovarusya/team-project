420e08476fff51a851d2737f4e20aa14b9de3bc5
Irina Rusinova <irina.rusinova19@gmail.com>
2023-10-17 13:01:00 +0300

    feature: add simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import HomePage from './components/home-page/HomePage';
+import SignInPage from './components/signin-page/SignInPage';
+import SignUpPage from './components/signup-page/SignUpPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Routes>
+        </Header>
+        </BrowserRouter>
     </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,18 @@
 import { FC, PropsWithChildren } from "react";
+import { Link } from "react-router-dom";
 
 
 const Header: FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
     <header>
       <h2>Header</h2>
+      <nav>
+        <ul>
+          <li><Link to="/">Logo</Link></li>
+          <li><Link to="/signin">Sign in</Link></li>
+          <li><Link to="/signup">Sign up</Link></li>
+        </ul>
+      </nav>
       {children}
     </header>
   );
