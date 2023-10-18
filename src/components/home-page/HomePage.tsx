c282c40489121734f95309ed17dabe42dffbea76
Irina Rusinova <irina.rusinova19@gmail.com>
2023-10-18 09:11:12 +0300

    feature: add sign out button

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -1,16 +1,18 @@
 import { FC, PropsWithChildren } from "react";
 import { useAuth } from "../../hooks/use-auth";
 import { Navigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../../store/slices/userSlice";
 
 
 const HomePage: FC<PropsWithChildren<{}>> = ({ children }) => {
-  const { isAuth } =  useAuth();
-
-  console.log("is auth? " + isAuth);
+  const { isAuth, email } =  useAuth();
+  const dispatch = useDispatch();
 
   return (isAuth ?
     <article>
       <h2>Home page</h2>
+      <button onClick={() => dispatch(removeUser())}>Sign out from {email}</button>
       {children}
     </article> : (
     <Navigate to="/signin" />
