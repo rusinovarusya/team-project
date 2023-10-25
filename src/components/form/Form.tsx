ef83f4be1779ea07cc5baa04537f7ce8f0f06302
Irina Rusinova <irina.rusinova19@gmail.com>
2023-10-25 13:35:11 +0300

    feature: add style to form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,5 @@
 import { FC, PropsWithChildren, useState } from "react";
+import "./index.css";
 
 interface FormProps {
   title: string;
@@ -11,10 +12,10 @@ const Form: FC<PropsWithChildren<FormProps>> = ({ title, handleClick }) => {
   const [password, setPassword] = useState("");
 
   return (
-    <div>
-      <input type="email" name="" id="" value={email} onChange={e => setEmail(e.target.value)} placeholder="email" />
-      <input type="password" name="" id="" value={password} onChange={e => setPassword(e.target.value)} placeholder="password" />
-      <button onClick={() => handleClick(email, password)}>{title}</button>
+    <div className="form-container">
+      <input className="form-input" type="email" name="user_email" value={email} onChange={e => setEmail(e.target.value)} placeholder="email" />
+      <input className="form-input" type="password" name="user_password" value={password} onChange={e => setPassword(e.target.value)} placeholder="password" />
+      <button className="form-btn" onClick={() => handleClick(email, password)}>{title}</button>
     </div>
   );
 }
