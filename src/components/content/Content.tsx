7e12b0da11cbb08d29af04d9448de93b4d2dd046
Alex Usin <79891938+LxCtchr@users.noreply.github.com>
2023-10-28 00:44:12 +0600

    [feature:] add films info on content page through new api link

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,51 +1,45 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+
+import searchFilmInstance from "../../API/axios";
+
+import { TMovie } from "../../redux/movie/type";
+
 import './index.css'
 
+const FILM_IDS : string[] = ['tt0111161', 'tt0068646', 'tt0468569', 'tt0071562', 'tt0108052', 'tt0167260', 'tt0110912', 'tt0050083']
+
 const Content: FC = () => {
-    
-    return (
-        <article className="content">
-            <ul className="content-items">
-                <li className="content-item">
-                    <h3 className="item-heading">Название фильма</h3>
-                    <img className="item-poster" src="https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=FILM+POSTER&font=kelson" width="200" height="300"></img>
-                    <p className="item-summary">Краткое описание фильма</p>
-                </li>
-                <li className="content-item">
-                    <h3 className="item-heading">Название фильма</h3>
-                    <img className="item-poster" src="https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=FILM+POSTER&font=kelson" width="200" height="300"></img>
-                    <p className="item-summary">Краткое описание фильма</p>
-                </li>
-                <li className="content-item">
-                    <h3 className="item-heading">Название фильма</h3>
-                    <img className="item-poster" src="https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=FILM+POSTER&font=kelson" width="200" height="300"></img>
-                    <p className="item-summary">Краткое описание фильма</p>
-                </li>
-                <li className="content-item">
-                    <h3 className="item-heading">Название фильма</h3>
-                    <img className="item-poster" src="https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=FILM+POSTER&font=kelson" width="200" height="300"></img>
-                    <p className="item-summary">Краткое описание фильма</p>
-                </li>
-                <li className="content-item">
-                    <h3 className="item-heading">Название фильма</h3>
-                    <img className="item-poster" src="https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=FILM+POSTER&font=kelson" width="200" height="300"></img>
-                    <p className="item-summary">Краткое описание фильма</p>
-                </li>
-                <li className="content-item">
-                    <h3 className="item-heading">Название фильма</h3>
-                    <img className="item-poster" src="https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=FILM+POSTER&font=kelson" width="200" height="300"></img>
-                    <p className="item-summary">Краткое описание фильма</p>
-                </li>
-                <li className="content-item">
-                    <h3 className="item-heading">Название фильма</h3>
-                    <img className="item-poster" src="https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=FILM+POSTER&font=kelson" width="200" height="300"></img>
-                    <p className="item-summary">Краткое описание фильма</p>
-                </li>
-                <li className="content-item">
-                    <h3 className="item-heading">Название фильма</h3>
-                    <img className="item-poster" src="https://imgholder.ru/200x300/8493a8/adb9ca.jpg&text=FILM+POSTER&font=kelson" width="200" height="300"></img>
-                    <p className="item-summary">Краткое описание фильма</p>
-                </li>
+  const [ids, setIds] = useState<string[]>(FILM_IDS);
+  const [movies, setMovies] = useState<TMovie[]>([]);
+  
+  useEffect(() => {
+    async function fetchMovie(id: string) {
+      await searchFilmInstance
+        .get(`?tt=${id}`)
+        .then((res) => {
+          setMovies((prevState) => [...prevState, {
+            name: res.data.short.name,
+            image: res.data.short.image,
+            description: res.data.short.description
+          }])
+        })
+    }
+
+    for (let i = 0; i < ids.length; i++) {
+      fetchMovie(ids[i]);
+    }
+  }, [ids])
+
+  return (
+    <article className="content">
+      <ul className="content-items">
+        {movies ? movies.map((movie) => (
+          <li className="content-item">
+            <h3 className="item-heading">{movie.name}</h3>
+            <img className="item-poster" src={movie.image} alt="movie poster" width="300px" />
+            <p className="item-summary">{movie.description}</p>
+          </li>
+        )) : "Не загрузилось"}
             </ul>
         </article>
     );
